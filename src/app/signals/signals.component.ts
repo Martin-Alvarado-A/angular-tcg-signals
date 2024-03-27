e9681e7004b6bd678304a89c50196b3124f96b46
martin <martin.alvarado2002@gmail.com>
2024-03-27 15:36:13 +0100

    [25-364]: Signal Updating: set(), update() & mutate()

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -8,16 +8,18 @@ import { Component, signal } from '@angular/core';
   imports: [NgFor],
 })
 export class SignalsComponent {
-  actions: string[] = [];
+  actions = signal<string[]>([]);
   counter = signal(0);
 
   increment() {
-    this.counter.update((oldCounter) => oldCounter + 1);
-    this.actions.push('INCREMENT');
+    this.counter.set(this.counter() + 1);
+    // mutate is deprecated since v17
+    this.actions.mutate((oldActions) => oldActions.push('INCREMENT'));
   }
 
   decrement() {
     this.counter.update((oldCounter) => oldCounter - 1);
-    this.actions.push('DECREMENT');
+    // Better way to update a signal
+    this.actions.update((oldActions) => [...oldActions, 'DECREMENT']);
   }
 }
